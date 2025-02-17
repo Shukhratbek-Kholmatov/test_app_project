ce5d8b0641848553485c4277f1170e1ab58f1003
Shuhrat <83421087+Shukhratbek-Kholmatov@users.noreply.github.com>
2025-02-17 20:29:13 +0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-email_password = env("email")
+email_password = env("email_password")
 email_user = env("email_user")
 
 # SECURITY WARNING: don't run with debug turned on in production!
